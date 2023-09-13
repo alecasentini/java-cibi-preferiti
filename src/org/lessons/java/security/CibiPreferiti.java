25c4d5e1f466107e94c7098d2049b79afc052ead
alecasentini <ale.casentini@gmail.com>
2023-09-13 17:22:54 +0200

    esercizio bonus completato

--- src/org/lessons/java/security/CibiPreferiti.java
+++ src/org/lessons/java/security/CibiPreferiti.java
@@ -3,5 +3,14 @@ package org.lessons.java.security;
 public class CibiPreferiti {
 	public static void main(String[] args) {
 		
+		String[] cibi = {"Pizza", "Hamburger", "Lasagne", "Cotoletta", "Tiramisu"};
+		
+		System.out.println("Numero cibi in classifica: " + cibi.length);
+		
+		System.out.println("Al primo posto: " + cibi[0]);
+		
+		System.out.println("All'ultimo posto: " + cibi[cibi.length-1]);
+		
+		System.out.println("In mezzo alla classifica: " + cibi[cibi.length/2]);
 	}
 }
